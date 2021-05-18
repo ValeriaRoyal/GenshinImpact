419474af7e3f742b622241fb6c6b273d32b3925a
Valéria Regina <valeriaroyal.contato@gmail.com>
2021-05-17 23:31:22 -0300

    Base alterada
    Adicionando nova estrutura do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+
+import { MaterialModule } from './shared/material/material.module';
+import { TopoComponent } from './shared/components/topo/topo.component';
+import { RodapeComponent } from './shared/components/rodape/rodape.component';
+import { AlertaComponent } from './shared/components/alerta/alerta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    AlertaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LayoutModule,
+    MaterialModule,
+    AppRoutingModule
   ],
-  providers: [],
+  entryComponents: [AlertaComponent],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
